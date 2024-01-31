453010f246e76fa5ca7d5254d0fd93eeee27b630
Ruan <a50064xxx@gmail.com>
2024-01-31 09:17:54 +0800

    fix promotionfindall import package

--- Shopping_Project/src/main/java/com/controller/PromotionFindAllServlet.java
+++ Shopping_Project/src/main/java/com/controller/PromotionFindAllServlet.java
@@ -16,10 +16,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
 import com.entity.Promotion;
-import com.entity.empPass;
 
 @WebServlet("/PromotionFindAllServlet")
 public class PromotionFindAllServlet extends HttpServlet {
